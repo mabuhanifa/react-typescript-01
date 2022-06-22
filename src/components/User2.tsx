2b003300c687a1b4621a6c54e3e3bf17ec32aa0a
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-22 20:55:01 +0600

    map used

--- src/components/User2.tsx
+++ src/components/User2.tsx
@@ -2,7 +2,11 @@ type UserProps2 = {
   lang: string[];
 };
 const User2 = ({ lang }: UserProps2) => {
-  return <div>hi</div>;
+  return <div>{
+    lang.map(lang => {
+      return <h1>{lang}</h1>
+    })
+  }</div>;
 };
 
 export default User2;
